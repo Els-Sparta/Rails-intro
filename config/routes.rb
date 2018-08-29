c08aba2dbc0b9672c5d4e0a06f1de39082a7109c
Els <Emathivannan@spartaglobal.com>
2018-08-29 09:42:15 +0100

    Rails-lab

---
+++ config/routes.rb
@@ -0,0 +1,39 @@
+Rails.application.routes.draw do
+
+  # get 'books/index'
+
+  get 'books/show'
+  #
+  # get 'books/create'
+  #
+  # get 'books/new'
+  #
+  get 'books/edit'
+  #
+  get 'books/update'
+  #
+  get 'books/destroy'
+  #
+  # get 'books/title'
+  #
+  # get 'books/price'
+
+  get 'books', to: "books#index"
+
+  get 'books/new', to: "books#new"
+
+  post 'books', to: "books#create"
+
+  get 'books/:id', to: "books#show"
+
+  get 'books/:id/edit', to: "books#edit"
+
+  put 'books/:id', to: "books#update"
+
+  delete 'books/:id', to: "books#destroy"
+
+  # BONUS
+  resources :books
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
